3ff7fc91158efaa7bf41fbff09d5fc11417b6a11
tiago <tiago@PROD-J03.zeta.dominio>
2013-03-05 08:02:29 -0300

    commit inicial

---
+++ Lab3/Lab3 Aula 02/src/Exercicios.java
@@ -0,0 +1,49 @@
+import java.lang.reflect.Array;
+
+import javax.swing.JOptionPane;
+
+public class Exercicios {
+
+	public void exercicio01(){
+		 
+		int numero = Integer.parseInt(JOptionPane.showInputDialog("Digite um número positivo: "));
+		String div = separarDivisiveis(numero);
+		JOptionPane.showMessageDialog(null,"Os divisores são: "+div);
+		
+	 }
+
+	private String separarDivisiveis(int numero) {
+
+		StringBuilder sb = new StringBuilder();
+		Integer[] array = new Integer[numero];
+		int cont = 0;
+		
+		if(numero > 0){
+			for (int i = 1; i <= numero; i++) {
+				if(numero % i == 0){
+					array[cont] = i;
+					cont++;
+				}
+			}
+			
+			if(cont > 0){
+				for (int i = 0; i < array.length; i++) {
+					if(i == 0){
+						sb.append(array[i]);
+					} else {
+						sb.append(String.format(", %s",  array[i]));
+					}
+				}
+				
+			} else {
+				sb.append("Sem divisores!");
+			}
+			
+		} else {
+			sb.append("Número inválido!");
+		}
+		
+		
+		return sb.toString();
+	}
+}
